dbd631237d1276a2fe72b8fc864be17abd33e3b2
Tetyana <tatt@Tanias-Mac.local>
2022-01-27 20:27:32 +0000

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # root "near#index"
   root 'pages#home'
+  get 'about', to: 'pages#about'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # get "/near/:id", to: "near#index"
 
